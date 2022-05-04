24c8458237598cca102d9d89ac05aa3dbe4fb9f9
Andrey Potyomkin <and_tem@icloud.com>
2022-05-04 09:59:26 +0300

    doc: добавлен базовый пример для code style

---
+++ examples/global.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="@astral/ui/declaration/emotion" />
+/// <reference types="@astral/ui/declaration/mui-material" />
+/// <reference types="@emotion/react/types/css-prop" />
---
+++ examples/src/components/User/User.tsx
@@ -0,0 +1,5 @@
+import { UserInfo } from './UserInfo';
+
+export const User = () => {
+  return <UserInfo title="title" userName="ivan" userEmail="ivan@ivan.com" />;
+};
