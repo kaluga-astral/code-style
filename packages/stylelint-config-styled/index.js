8608c25e4b1ad878c3eef39ec63f9e8130cf8ef5
Andrey Potyomkin <and_tem@icloud.com>
2022-07-21 09:32:44 +0300

    bug(UIKIT-334,stylelint-config-styled): Исправлена ошибка при работе конфига на windows (#25)

--- packages/stylelint-config-styled/index.js
+++ packages/stylelint-config-styled/index.js
@@ -1,47 +1,43 @@
 const path = require('path');
 
 module.exports = {
-  overrides: [
-    {
-      files: [
-        `${path.sep}**${path.sep}*.ts`,
-        `${path.sep}**${path.sep}*.tsx`,
-        `${path.sep}**${path.sep}*.js`,
-        `${path.sep}**${path.sep}*.jsx`,
-      ],
-
-      customSyntax: '@stylelint/postcss-css-in-js',
-
-      extends: [
-        'stylelint-config-standard',
-        // группирует правила по блокам
-        'stylelint-config-rational-order',
-        'stylelint-prettier/recommended',
-      ],
-
-      plugins: ['stylelint-prettier', 'stylelint-order'],
-
-      rules: {
-        // отключено из-за несовместимости со styled
-        'function-no-unknown': null,
-        // отключено из-за несовместимости со styled
-        'function-name-case': null,
-        // отключено из-за несовместимости со styled
-        'value-keyword-case': null,
-
-        // разрешен kebab case (используется в mui)
-        'selector-class-pattern':
-          '(([A-Z][a-z0-9]+)*)+(_[a-z0-9]+([A-Z][a-z0-9]+)*)?(-[a-z0-9]+([A-Z][a-z0-9]+)*)?',
-        'plugin/rational-order': [
-          true,
-          {
-            'empty-line-between-groups': true,
-          },
-        ],
-
-        // отключено из-за конфликта с plugin/rational-order
-        'declaration-empty-line-before': null,
-      },
-    },
+  files: [
+    `${path.sep}**${path.sep}*.ts`,
+    `${path.sep}**${path.sep}*.tsx`,
+    `${path.sep}**${path.sep}*.js`,
+    `${path.sep}**${path.sep}*.jsx`,
   ],
+
+  customSyntax: '@stylelint/postcss-css-in-js',
+
+  extends: [
+    'stylelint-config-standard',
+    // группирует правила по блокам
+    'stylelint-config-rational-order',
+    'stylelint-prettier/recommended',
+  ],
+
+  plugins: ['stylelint-prettier', 'stylelint-order'],
+
+  rules: {
+    // отключено из-за несовместимости со styled
+    'function-no-unknown': null,
+    // отключено из-за несовместимости со styled
+    'function-name-case': null,
+    // отключено из-за несовместимости со styled
+    'value-keyword-case': null,
+
+    // разрешен kebab case (используется в mui)
+    'selector-class-pattern':
+      '(([A-Z][a-z0-9]+)*)+(_[a-z0-9]+([A-Z][a-z0-9]+)*)?(-[a-z0-9]+([A-Z][a-z0-9]+)*)?',
+    'plugin/rational-order': [
+      true,
+      {
+        'empty-line-between-groups': true,
+      },
+    ],
+
+    // отключено из-за конфликта с plugin/rational-order
+    'declaration-empty-line-before': null,
+  },
 };
