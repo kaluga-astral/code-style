077712ba705d87e373cbf2523c4631bedb3132ed
Andrey Potyomkin <and_tem@icloud.com>
2022-07-21 17:37:42 +0300

    chore(UIKIT-334): Тест билда (#30)
    * chore: Исправлена модификация package.json при билде
    
    * chore(UIKIT-334): Билд пакетов выделен в отдельный шаг
    
    * chore(UIKIT-334): Исправлены пути для копирования файлов
    
    * chore(UIKIT-334): Prebuild перенесен в js
    
    * chore(UIKIT-334): Тест билда

--- commander/tasks/prebuild.js
+++ commander/tasks/prebuild.js
@@ -6,6 +6,8 @@ const { DIST_DIR_NAME } = require('../constants');
 const prebuild = () => {
   console.log('Starting prebuild...');
   console.log('Remove lib...');
+  // test
+  console.log(path.join(process.cwd(), DIST_DIR_NAME));
 
   fs.rmSync(path.join(process.cwd(), DIST_DIR_NAME), {
     recursive: true,
